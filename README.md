237b2ce2856aff09d4b5891defe669a99940d8c9
James Taylor <54148103+jatayl@users.noreply.github.com>
2022-06-24 23:56:59 -0400

    added information in readmes and made some minor updates

---
+++ README.md
@@ -0,0 +1,4 @@
+# rust-dlc examples
+
+* sports-betting (beginner): the most simple type of discreet log contract with only two outcomes
+* degen-leverage (advanced) [TODO]: learn how to do efficient numerical outcome decomposition, multi-oracle redudency and oracle error tolerance
--- sports-betting/main.rs
+++ sports-betting/main.rs
@@ -2,7 +2,7 @@ extern crate bitcoin;
 extern crate dlc;
 extern crate secp256k1_zkp;
 
-use bitcoin::{OutPoint, Script};
+use bitcoin::{Address, Network, OutPoint, Script};
 use dlc::{OracleInfo, PartyParams, Payout, TxInputInfo};
 use secp256k1_zkp::{
     bitcoin_hashes::sha256::Hash as Sha256,
@@ -13,8 +13,16 @@ use secp256k1_zkp::{
     All, Message, PublicKey, Secp256k1, SecretKey,
 };
 
-fn generate_new_address() -> Script {
-    Script::new()
+fn generate_new_address(secp: &Secp256k1<All>, rng: &mut ThreadRng) -> Script {
+    let sk = bitcoin::PrivateKey {
+        key: SecretKey::new(rng),
+        network: Network::Testnet,
+        compressed: true,
+    };
+    let pk = bitcoin::PublicKey::from_private_key(secp, &sk);
+    Address::p2wpkh(&pk, Network::Testnet)
+        .unwrap()
+        .script_pubkey()
 }
 
 fn create_counterparty(
@@ -24,12 +32,22 @@ fn create_counterparty(
     collateral: u64,
     id: u64,
 ) -> (PartyParams, SecretKey) {
+    // input_amount is the amount of bitcoin that the party's inputs amount to
+    // collateral is the amount of bitcoin that will be going into the contract
+    // (input_amount - collateral) will be given back in the funding transaction as
+    // change
+
     let secret_key = SecretKey::new(rng);
     let public_key = PublicKey::from_secret_key(&secp, &secret_key);
 
-    let change_script_pubkey = generate_new_address();
-    let payout_script_pubkey = generate_new_address();
+    // specify where to send change and where to send the final payout for this party
+    let change_script_pubkey = generate_new_address(secp, rng);
+    let payout_script_pubkey = generate_new_address(secp, rng);
 
+    // specify information about the inputs for this lender, it is possible that there will
+    // be several of these inputs for each party which is why you should specify a `serial_id` for
+    // each input for ordering purposes in the final transaction. because this tutorial assumes
+    // only one input for each party, we can trivially set this as so
     let input = TxInputInfo {
         outpoint: OutPoint::null(),
         max_witness_len: 108,
@@ -57,15 +75,16 @@ fn create_counterparty(
 
 fn sports_bet_payout() -> Vec<Payout> {
     // denote alice and bob as offerer and accepter respectively
-    let duke_wins = Payout {
-        offer: 0,
-        accept: 100_000,
-    };
+    // alice is betting on unc winning and bob is betting on duke winning
     let unc_wins = Payout {
         offer: 100_000,
         accept: 0,
     };
-    vec![duke_wins, unc_wins]
+    let duke_wins = Payout {
+        offer: 0,
+        accept: 100_000,
+    };
+    vec![unc_wins, duke_wins]
 }
 
 struct OracleSecret {
@@ -74,6 +93,9 @@ struct OracleSecret {
 }
 
 fn get_oracle_details(secp: &Secp256k1<All>, rng: &mut ThreadRng) -> (OracleInfo, OracleSecret) {
+    // unless you are curious about how oracles work, understanding this code is not incredibly
+    // important. however, it is important to retrieve nonces and an oracle public key to construct
+    // an `OracleInfo` struct
     let (oracle_kp, oracle_pubkey) = secp.generate_schnorrsig_keypair(rng);
 
     let mut sk_nonce = [0u8; 32];
@@ -114,8 +136,8 @@ fn oracle_attest_to_event(
 #[derive(Clone, Copy)]
 #[repr(u8)]
 enum Winner {
-    Unc,
     Duke,
+    Unc,
 }
 
 fn main() {
@@ -154,7 +176,12 @@ fn main() {
     )
     .expect("error generating dlc transactions");
 
-    // generate all of the messages that could occur
+    // generate all of the messages that could occur. this vector is 3-dimensional because there
+    // are possibilities for several oracles and several digits for each outcomes (not relevant for
+    // enumerated outcomes) that is, [events][oracles][digits]
+    // this must match the ordering in the payouts--because the payout for alice and unc is first,
+    // it must also be first here. all adaptor signatures and cets will be ordered correspondingly
+    // as well
     let messages = vec![
         vec![vec![Message::from_hashed_data::<Sha256>(&[
             Winner::Unc as u8
@@ -191,13 +218,12 @@ fn main() {
             &alice_params.fund_pubkey,
             &funding_script_pubkey,
             fund_output_value,
-            &messages[i],
+            &messages[(i + 1) / 2],
         )
-        .expect("invalid cet!");
+        .expect("invalid adaptor signature!");
     }
 
-    // in reality, bob would generate adaptor signatures just as alice did for alice to verify and store in the event
-    // she wants the ability to close the contract
+    // in reality, bob would generate adaptor signatures just as alice did for alice to verify and store
 
     // Now broadcast the funding transaction to the bitcoin blockchain
     let funding_transaction = &dlc_transactions.fund;
@@ -206,18 +232,18 @@ fn main() {
     // the game occurs, let's see what the oracle said!
     let (winner, oracle_signature) = oracle_attest_to_event(&secp, &oracle_secret);
 
+    // if the winner is duke, then bob wins and so take the payout that favors him
     let mut valid_cet = match winner {
-        Winner::Duke => cets[1].clone(),
         Winner::Unc => cets[0].clone(),
+        Winner::Duke => cets[1].clone(),
     };
 
     // because duke won, bob can take alice's adaptor signatures and oracle signature to produce a
     // valid signed cet
-
     dlc::sign_cet(
         &secp,
         &mut valid_cet,
-        &alice_adaptor_sigs[0],
+        &alice_adaptor_sigs[1],
         &[vec![oracle_signature]],
         &bob_secret,
         &alice_params.fund_pubkey,
@@ -226,11 +252,10 @@ fn main() {
     )
     .unwrap();
 
-    blockchain.broadcast(&valid_cet);
+    // if something bad were to happen (e.g. oracle does not attest to event), then alice and bob
+    // can get their collateral back with the refund transaction
+    // blockchain.broadcast(&dlc_transactions.refund);
 }
-
-// TODO: put mock blockchain in a library shared between all tutorails
-// perhaps can make it check if transaction is valid and store them
 
 use bitcoin::Transaction;
 
@@ -242,6 +267,6 @@ impl MockBlockchain {
     }
 
     fn broadcast(&self, _transaction: &Transaction) {
-        // noop!
+        // this isn't actually a blockchain! i wonder if they fell for it
     }
 }
